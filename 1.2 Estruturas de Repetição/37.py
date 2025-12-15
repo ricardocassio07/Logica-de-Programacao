a95d904be33ee219d90ee6a1de2d432b47335072
cássio <ricardocassio271@gmail.com>
2025-12-15 13:53:06 -0300

    Lógica de Programação

--- 1.2 Estruturas de Repetição/37.py
+++ 1.2 Estruturas de Repetição/37.py
@@ -3,9 +3,8 @@ n = (int(input("Digite um número inteiro: ")))
 x = 0
 y = 1
 for i in range(1, (n + 1)):
-    resultado = (x + y)
-    print("O {}º termo da série é {}".format(i, resultado))
-    if (i % 2 == 0):
-        x = resultado
-    else:
-        y = resultado
+    temp = x
+    x += y
+    y = temp
+print("O {}º elemento da série é {}!".format(n, x))
+    
